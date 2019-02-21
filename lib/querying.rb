bbba489adce2f11867ac90185d98ec7ab7143b16
Natalie Halawani <hala1224@gmail.com>
2019-02-21 01:50:35 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,7 +41,10 @@ def select_series_title_with_most_human_characters
   FROM series
   LEFT OUTER JOIN characters
   ON characters.series_id=series.id
-  WHERE characters.species='human' "
+  WHERE characters.species='human' 
+  GROUP BY series.title
+  GROUP BY COUNT(characters.species='human') DESC 
+  LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
