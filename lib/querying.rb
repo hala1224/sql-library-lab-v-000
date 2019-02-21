886c7a40c0647f1a23dfc6f4dcd39d5747ff9756
Natalie Halawani <hala1224@gmail.com>
2019-02-21 01:34:21 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,13 +23,13 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "Select authors.name, subgenres.name
-  # FROM authors 
-  # JOIN series 
-  # ON series.author_id = authors.id 
-  # JOIN subgenres 
-  # ON series.subgenre_id = subgenres.id"
-  FROM subgenres
-  LEFT
+  FROM authors 
+  JOIN series 
+  ON series.author_id = authors.id 
+  JOIN subgenres 
+  ON series.subgenre_id = subgenres.id"
+  # FROM subgenres
+  # LEFT
 end
 
 def select_series_title_with_most_human_characters
