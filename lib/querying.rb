baa71257105d1100619bfc981635d60e22761a38
Natalie Halawani <hala1224@gmail.com>
2019-02-21 01:09:44 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,14 +1,24 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year FROM books WHERE series_id = 1 ORDER BY books.year "
+  "SELECT title, year 
+  FROM books 
+  WHERE series_id = 1 
+  ORDER BY books.year "
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1"
+  "SELECT name, motto 
+  FROM characters 
+  ORDER BY LENGTH(motto) DESC 
+  LIMIT 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species)
+  FROM characters
+  GROUP by species 
+  ORDER BY COUNT(species) DESC
+  LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
